3e37334919fb6f3b2405732a8af4375d21fb70af
Dennis Wölfing <denniswoelfing@gmx.de>
2016-04-08 16:07:58 +0200

    Implement functions to allocate consecutive ranges of pages.

--- kernel/include/dennix/kernel/addressspace.h
+++ kernel/include/dennix/kernel/addressspace.h
@@ -28,10 +28,15 @@
 
 class AddressSpace {
 public:
+    vaddr_t allocate(size_t nPages);
+    void free(vaddr_t virtualAddress, size_t nPages);
     paddr_t getPhysicalAddress(vaddr_t virtualAddress);
     vaddr_t map(paddr_t physicalAddress, int flags);
     vaddr_t mapAt(vaddr_t virtualAddress, paddr_t physicalAddress, int flags);
     vaddr_t mapAt(size_t pdIndex, size_t ptIndex, paddr_t physicalAddress,
+            int flags);
+    vaddr_t mapRange(paddr_t* physicalAddresses, int flags);
+    vaddr_t mapRangeAt(vaddr_t virtualAddress, paddr_t* physicalAddresses,
             int flags);
     void unmap(vaddr_t virtualAddress);
 private:
--- kernel/src/addressspace.cpp
+++ kernel/src/addressspace.cpp
@@ -20,6 +20,7 @@
 #include <dennix/kernel/addressspace.h>
 #include <dennix/kernel/kernel.h>
 #include <dennix/kernel/log.h>
+#include <dennix/kernel/physicalmemory.h>
 
 #define RECURSIVE_MAPPING 0xFFC00000
 
@@ -57,6 +58,27 @@ void AddressSpace::initialize() {
     }
 }
 
+vaddr_t AddressSpace::allocate(size_t nPages) {
+    paddr_t physicalAddresses[nPages + 1];
+
+    for (size_t i = 0; i < nPages; i++) {
+        physicalAddresses[i] = PhysicalMemory::popPageFrame();
+        if (!physicalAddresses[i]) return 0;
+    }
+    physicalAddresses[nPages] = 0;
+
+    return mapRange(physicalAddresses, PAGE_PRESENT | PAGE_WRITABLE);
+}
+
+void AddressSpace::free(vaddr_t virtualAddress, size_t nPages) {
+    for (size_t i = 0; i < nPages; i++) {
+        paddr_t physicalAddress = getPhysicalAddress(virtualAddress);
+        unmap(virtualAddress);
+        PhysicalMemory::pushPageFrame(physicalAddress);
+        virtualAddress += 0x1000;
+    }
+}
+
 paddr_t AddressSpace::getPhysicalAddress(vaddr_t virtualAddress) {
     size_t pdIndex;
     size_t ptIndex;
@@ -83,6 +105,9 @@ paddr_t AddressSpace::getPhysicalAddress(vaddr_t virtualAddress) {
 vaddr_t AddressSpace::map(paddr_t physicalAddress, int flags) {
     // Find a free page in the higher half and map it
     for (size_t pdIndex = 0x300; pdIndex < 0x400; pdIndex++) {
+        uintptr_t* pageTable = (uintptr_t*)
+                (RECURSIVE_MAPPING + 0x3FF000 + 4 * pdIndex);
+        if (!*pageTable) continue;
         for (size_t ptIndex = 0; ptIndex < 0x400; ptIndex++) {
             uintptr_t* pageEntry = (uintptr_t*)
                     (RECURSIVE_MAPPING + 0x1000 * pdIndex + 4 * ptIndex);
@@ -96,7 +121,7 @@ vaddr_t AddressSpace::map(paddr_t physicalAddress, int flags) {
 }
 
 vaddr_t AddressSpace::mapAt(
-        vaddr_t virtualAddress,paddr_t physicalAddress, int flags) {
+        vaddr_t virtualAddress, paddr_t physicalAddress, int flags) {
     size_t pdIndex;
     size_t ptIndex;
     addressToIndex(virtualAddress, pdIndex, ptIndex);
@@ -107,16 +132,22 @@ vaddr_t AddressSpace::mapAt(
         size_t pdIndex, size_t ptIndex, paddr_t physicalAddress, int flags) {
     if (this == kernelSpace) {
         uintptr_t* pageTable = (uintptr_t*)
-                        (RECURSIVE_MAPPING + 0x3FF000 + 4 * pdIndex);
-        if (*pageTable) {
-            uintptr_t* pageEntry = (uintptr_t*)
-                    (RECURSIVE_MAPPING + 0x1000 * pdIndex + 4 * ptIndex);
-            *pageEntry = physicalAddress | flags;
-        } else {
-            //TODO: Allocate a new page table.
-            Log::printf("Error: Page Table does not exist.\n");
-            return 0;
+                (RECURSIVE_MAPPING + 0x3FF000 + 4 * pdIndex);
+        uintptr_t* pageEntry = (uintptr_t*)
+                (RECURSIVE_MAPPING + 0x1000 * pdIndex + 4 * ptIndex);
+
+        if (!*pageTable) {
+            // Allocate a new page table and map it in the page directory
+            paddr_t pageTablePhys = PhysicalMemory::popPageFrame();
+            uintptr_t* pageTableVirt = (uintptr_t*) mapAt(0x3FF, pdIndex,
+                    pageTablePhys, PAGE_PRESENT | PAGE_WRITABLE);
+
+            for(size_t i = 0; i < 1024; i++) {
+                pageTableVirt[i] = 0;
+            }
         }
+
+        *pageEntry = physicalAddress | flags;
     } else {
         //TODO: Implement this for other address spaces.
         return 0;
@@ -126,6 +157,70 @@ vaddr_t AddressSpace::mapAt(
 
     // Flush the TLB
     asm volatile ("invlpg (%0)" :: "r"(virtualAddress));
+
+    return virtualAddress;
+}
+
+vaddr_t AddressSpace::mapRange(paddr_t* physicalAddresses, int flags) {
+    paddr_t* phys = physicalAddresses;
+    size_t nPages = 0;
+
+    while (*phys++){
+        nPages++;
+    }
+
+    // Find enough free pages in the higher half and map them
+    for (size_t pdIndex = 0x300; pdIndex < 0x400; pdIndex++) {
+        for (size_t ptIndex = 0; ptIndex < 0x400; ptIndex++) {
+            size_t pd = pdIndex;
+            size_t pt = ptIndex;
+            size_t foundPages = 0;
+
+            while (foundPages <= nPages) {
+                uintptr_t* pageTable = (uintptr_t*)
+                        (RECURSIVE_MAPPING + 0x3FF000 + 4 * pd);
+                uintptr_t* pageEntry = (uintptr_t*)
+                        (RECURSIVE_MAPPING + 0x1000 * pd + 4 * pt);
+
+                if (!*pageTable) {
+                    foundPages += 1024;
+                    pd++;
+                    pt = 0;
+                    continue;
+                }
+
+                if (*pageEntry) break;
+
+                pt++;
+                foundPages++;
+
+                if (pt >= 1024) {
+                    pd++;
+                    pt = 0;
+                }
+            }
+
+            if (foundPages >= nPages) {
+                return mapRangeAt(indexToAddress(pdIndex, ptIndex),
+                        physicalAddresses, flags);
+            }
+        }
+    }
+
+    return 0;
+}
+
+vaddr_t AddressSpace::mapRangeAt(vaddr_t virtualAddress,
+        paddr_t* physicalAddresses, int flags) {
+    vaddr_t addr = virtualAddress;
+
+    while (*physicalAddresses) {
+        if (!mapAt(addr, *physicalAddresses, flags)) {
+            return 0;
+        }
+        addr += 0x1000;
+        physicalAddresses++;
+    }
 
     return virtualAddress;
 }
