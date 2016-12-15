785f1a41ef00a0915306a35d73682c8fe973f965
Dennis Wölfing <denniswoelfing@gmx.de>
2016-12-15 16:58:58 +0100

    Build Dennix on Travis CI.

--- build-aux/install-toolchain.sh
+++ build-aux/install-toolchain.sh
@@ -28,6 +28,8 @@ SYSROOT: The system root containing the system headers.
          This variable must be set. Using the make install-toolchain target of
          the root Makefile will set this variable automatically.'
 
+set -e
+
 binutils_repo=https://github.com/dennis95/dennix-binutils.git
 gcc_repo=https://github.com/dennis95/dennix-gcc.git
 
@@ -36,26 +38,42 @@ gcc_repo=https://github.com/dennis95/dennix-gcc.git
 # Set some default values.
 [ -z "${PREFIX+x}" ] && PREFIX="$HOME/dennix-toolchain"
 [ -z "$SRCDIR" ] && SRCDIR="$HOME/src"
-[ -z "$BUILDDIR" ] &&  BUILDDIR="$SRCDIR"
+[ -z "$BUILDDIR" ] && BUILDDIR="$SRCDIR"
 [ -z "$ARCH" ] && ARCH=i686
 [ -z "$TARGET" ] && TARGET=$ARCH-dennix
 
 [ -z "$SYSROOT" ] && echo "Error: \$SYSROOT not set" && exit 1
+
+if [ "$CONTINUOUS_INTEGRATION" = true ]
+then
+    # When built by Travis CI check whether the toolchain needs to be rebuilt.
+    current_binutils=$(cat "$PREFIX/binutils-commit" || echo Not installed)
+    current_gcc=$(cat "$PREFIX/gcc-commit" || echo Not installed)
+    latest_binutils=$(git ls-remote $binutils_repo HEAD | cut -c 1-40)
+    latest_gcc=$(git ls-remote $gcc_repo HEAD | cut -c 1-40)
+    [ "$current_binutils" = "$latest_binutils" ] && \
+        [ "$current_gcc" = "$latest_gcc" ] && \
+        echo Cached Toolchain is already up to date. && exit
+    rm -rf "$PREFIX"
+fi
 
 # Make $SYSROOT an absolute path
 SYSROOT="$(cd "$SYSROOT" && pwd)"
 
 export PATH="$PREFIX/bin:$PATH"
 
+rm -rf "$SRCDIR/dennix-binutils" "$SRCDIR/dennix-gcc"
+rm -rf "$BUILDDIR/build-binutils" "$BUILDDIR/build-gcc"
+
 echo Downloading sources...
 mkdir -p "$SRCDIR"
-cd "$SRCDIR" || exit $?
+cd "$SRCDIR"
 git clone $binutils_repo dennix-binutils
 git clone $gcc_repo dennix-gcc
 
 echo Building binutils...
 mkdir -p "$BUILDDIR/build-binutils"
-cd "$BUILDDIR/build-binutils" || exit $?
+cd "$BUILDDIR/build-binutils"
 "$SRCDIR/dennix-binutils/configure" --target=$TARGET --prefix="$PREFIX" \
   --with-sysroot="$SYSROOT" --disable-werror --disable-nls
 make
@@ -63,10 +81,18 @@ make install
 
 echo Building gcc...
 mkdir -p "$BUILDDIR/build-gcc"
-cd "$BUILDDIR/build-gcc" || exit $?
+cd "$BUILDDIR/build-gcc"
 "$SRCDIR/dennix-gcc/configure" --target=$TARGET --prefix="$PREFIX" \
   --with-sysroot="$SYSROOT" --enable-languages=c,c++ --disable-nls
 make all-gcc all-target-libgcc
 make install-gcc install-target-libgcc
+
+if [ "$CONTINUOUS_INTEGRATION" = true ]
+then
+    cd "$SRCDIR/dennix-binutils"
+    git rev-parse HEAD > "$PREFIX/binutils-commit"
+    cd "$SRCDIR/dennix-gcc"
+    git rev-parse HEAD > "$PREFIX/gcc-commit"
+fi
 
 echo Installation completed.
