3244c85f42e515afd4e7230214db180ed0309ca7
Dennis Wölfing <denniswoelfing@gmx.de>
2018-05-29 15:04:36 +0200

    Print entries in a grid in ls(1) when interactive.

--- utils/ls.c
+++ utils/ls.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016, 2017 Dennis Wölfing
+/* Copyright (c) 2016, 2017, 2018 Dennis Wölfing
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -22,10 +22,15 @@
 #include <err.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
 
+static int filterDot(const struct dirent* dirent);
 static void listDirectory(const char* path);
+static void printEntriesGrid(struct dirent** list, int numEntries);
+static void printEntriesOneline(struct dirent** list, int numEntries);
 
 int main(int argc, char* argv[]) {
     struct option longopts[] = {
@@ -65,23 +70,78 @@ int main(int argc, char* argv[]) {
     }
 }
 
+static int filterDot(const struct dirent* dirent) {
+    if (dirent->d_name[0] == '.') {
+        return 0;
+    }
+    return 1;
+}
+
 static void listDirectory(const char* path) {
-    DIR* dir = opendir(path);
-    if (!dir) {
-        warn("opendir: '%s'", path);
+    struct dirent** list;
+    int numEntries = scandir(path, &list, filterDot, alphasort);
+    if (numEntries < 0) {
+        warn("scandir: '%s'", path);
         return;
     }
 
-    struct dirent* entry = readdir(dir);
-
-    while (entry) {
-        if (strcmp(entry->d_name, ".") != 0 &&
-                strcmp(entry->d_name, "..") != 0) {
-            puts(entry->d_name);
-        }
-
-        entry = readdir(dir);
+    if (isatty(1)) {
+        printEntriesGrid(list, numEntries);
+    } else {
+        printEntriesOneline(list, numEntries);
     }
 
-    closedir(dir);
+    for (int i = 0; i < numEntries; i++) {
+        free(list[i]);
+    }
+    free(list);
+}
+
+static void printEntriesGrid(struct dirent** list, int numEntries) {
+    static const size_t lineWidth = 80;
+    size_t maxLength = 0;
+
+    for (int i = 0; i < numEntries; i++) {
+        size_t length = strlen(list[i]->d_name);
+        if (length > maxLength) {
+            maxLength = length;
+        }
+    }
+
+    size_t columns = (lineWidth + 1) / (maxLength + 1);
+    if (columns == 0) {
+        columns = 1;
+    }
+
+    size_t lines = (numEntries + columns - 1) / columns;
+
+    size_t columnWidth = lineWidth / columns;
+
+    for (size_t line = 0; line < lines; line++) {
+        for (size_t column = 0; column < columns; column++) {
+            size_t index = column * lines + line;
+            if (index >= (size_t) numEntries) {
+                putchar('\n');
+                break;
+            }
+
+            struct dirent* dirent = list[index];
+            size_t length = strlen(dirent->d_name);
+            fputs(dirent->d_name, stdout);
+
+            if (column == columns - 1) {
+                putchar('\n');
+            } else {
+                for (size_t i = length; i < columnWidth; i++) {
+                    putchar(' ');
+                }
+            }
+        }
+    }
+}
+
+static void printEntriesOneline(struct dirent** list, int numEntries) {
+    for (int i = 0; i < numEntries; i++) {
+        puts(list[i]->d_name);
+    }
 }
