10973ff63fcfc4ae2345434eece62f58d0e502ec
Dennis Wölfing <denniswoelfing@gmx.de>
2018-07-11 19:01:17 +0200

    Enhance vcbprintf(3).
    The *printf functions now support flags, field widths, precisions and
    the %n specifier.

--- libc/src/stdio/vcbprintf.c
+++ libc/src/stdio/vcbprintf.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016, 2017 Dennis Wölfing
+/* Copyright (c) 2016, 2017, 2018 Dennis Wölfing
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,9 +14,11 @@
  */
 
 /* libc/src/stdio/vcbprintf.c
- * Print format.
+ * Print formatted output.
  */
 
+#include <errno.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -24,12 +26,20 @@
 #include <stdint.h>
 #include <string.h>
 
+// TODO: Support numbered arguments
+
 static const char* lowerDigits = "0123456789abcdef";
 static const char* upperDigits = "0123456789ABCDEF";
 
-static size_t integerToString(char* output, uintmax_t value, unsigned int base,
+static int integerToString(char* output, uintmax_t value, unsigned int base,
         const char* digits);
 static size_t noop(void*, const char*, size_t);
+static int printInteger(void* param,
+        size_t (*callback)(void*, const char*, size_t), char specifier,
+        uintmax_t value, int fieldWidth, int precision, int flags);
+static int printString(void* param,
+        size_t (*callback)(void*, const char*, size_t), const char* s,
+        int length, int fieldWidth, int flags);
 
 enum {
     LENGTH_CHAR,
@@ -44,16 +54,14 @@ enum {
 };
 
 enum {
-    SPECIFIER_SIGNED = 1,
-    SPECIFIER_OCTAL,
-    SPECIFIER_UNSIGNED,
-    SPECIFIER_HEX,
-    SPECIFIER_HEX_CAPITAL,
-    SPECIFIER_CHAR,
-    SPECIFIER_STRING,
-    SPECIFIER_POINTER,
-    SPECIFIER_N,
-    SPECIFIER_PERCENT_SIGN
+    // Thousands grouping is currently not implemented because it has no effect
+    // in POSIX locale.
+    FLAG_THOUSANDS_GROUPING = 1 << 0,
+    FLAG_LEFT_JUSTIFIED = 1 << 1,
+    FLAG_PLUS = 1 << 2,
+    FLAG_SPACE = 1 << 3,
+    FLAG_ALTERNATIVE = 1 << 4,
+    FLAG_LEADING_ZEROS = 1 << 5
 };
 
 int vcbprintf(void* param, size_t (*callback)(void*, const char*, size_t),
@@ -62,30 +70,79 @@ int vcbprintf(void* param, size_t (*callback)(void*, const char*, size_t),
         callback = noop;
     }
 
-    char buffer[sizeof(uintmax_t) * 3];
+    bool invalidConversion = false;
     int result = 0;
+#define INCREMENT_RESULT(x) \
+        if (__builtin_add_overflow(result, (x), &result)) goto overflow
 
     while (*format) {
-        if (*format != '%') {
+        if (*format != '%' || invalidConversion) {
             if (callback(param, format, 1) != 1) return -1;
-            result += 1;
+            INCREMENT_RESULT(1);
         } else {
-            int length = LENGTH_INT;
-            int specifier = 0;
+            const char* specifierBegin = format;
 
-            while (specifier == 0) {
+            int flags = 0;
+            while (true) {
                 switch (*++format) {
-                // TODO: Support numbered arguments
-                // TODO: Parse flags
-                // TODO: Parse field widths
+                case '\'': flags |= FLAG_THOUSANDS_GROUPING; continue;
+                case '-': flags |= FLAG_LEFT_JUSTIFIED; continue;
+                case '+': flags |= FLAG_PLUS; continue;
+                case ' ': flags |= FLAG_SPACE; continue;
+                case '#': flags |= FLAG_ALTERNATIVE; continue;
+                case '0': flags |= FLAG_LEADING_ZEROS; continue;
+                }
+                break;
+            }
 
+            int fieldWidth = 0;
+            if (*format == '*') {
+                fieldWidth = va_arg(ap, int);
+                if (fieldWidth < 0) {
+                    flags |= FLAG_LEFT_JUSTIFIED;
+                    fieldWidth = -fieldWidth;
+                }
+                format++;
+            } else {
+                while (*format >= '0' && *format <= '9') {
+                    fieldWidth = fieldWidth * 10 + *format - '0';
+                    format++;
+                }
+            }
+
+            int precision = -1;
+            if (*format == '.') {
+                format++;
+                if (*format == '*') {
+                    precision = va_arg(ap, int);
+                    format++;
+                } else {
+                    precision = 0;
+                    while (*format >= '0' && *format <= '9') {
+                        precision = precision * 10 + *format - '0';
+                        format++;
+                    }
+                }
+            }
+
+            int length = LENGTH_INT;
+            while (true) {
+                switch (*format++) {
                 // Parse length modifiers
                 case 'h':
-                    length = length == LENGTH_SHORT ? LENGTH_CHAR : LENGTH_SHORT;
-                    break;
+                    if (length == LENGTH_SHORT) {
+                        length = LENGTH_CHAR;
+                        break;
+                    }
+                    length = LENGTH_SHORT;
+                    continue;
                 case 'l':
-                    length = length == LENGTH_LONG ? LENGTH_LONG_LONG : LENGTH_LONG;
-                    break;
+                    if (length == LENGTH_LONG) {
+                        length  = LENGTH_LONG_LONG;
+                        break;
+                    }
+                    length = LENGTH_LONG;
+                    continue;
                 case 'j':
                     length = LENGTH_INTMAX;
                     break;
@@ -98,54 +155,17 @@ int vcbprintf(void* param, size_t (*callback)(void*, const char*, size_t),
                 case 'L':
                     length = LENGTH_LONG_DOUBLE;
                     break;
-
-                // Parse specifiers
-                case '%':
-                    specifier = SPECIFIER_PERCENT_SIGN;
-                    break;
-                case 'd':
-                case 'i':
-                    specifier = SPECIFIER_SIGNED;
-                    break;
-                case 'o':
-                    specifier = SPECIFIER_OCTAL;
-                    break;
-                case 'u':
-                    specifier = SPECIFIER_UNSIGNED;
-                    break;
-                case 'x':
-                    specifier = SPECIFIER_HEX;
-                    break;
-                case 'X':
-                    specifier = SPECIFIER_HEX_CAPITAL;
-                    break;
-                case 'c':
-                    specifier = SPECIFIER_CHAR;
-                    break;
-                case 's':
-                    specifier = SPECIFIER_STRING;
-                    break;
-                case 'p':
-                    specifier = SPECIFIER_POINTER;
-                    break;
-                case 'n':
-                    specifier = SPECIFIER_N;
-                    break;
-                case '\0':
-                    if (callback(param, "%", 1) != 1) return -1;
-                    return result + 1;
                 default:
-                    break;
+                    format--;
                 }
+                break;
             }
 
-            bool negative = false;
-            uintmax_t value;
+            char specifier = *format;
+            uintmax_t value = 0;
 
-            if (specifier == SPECIFIER_OCTAL
-                    || specifier == SPECIFIER_UNSIGNED
-                    || specifier == SPECIFIER_HEX
-                    || specifier == SPECIFIER_HEX_CAPITAL) {
+            if (specifier == 'o' || specifier == 'u' || specifier == 'x' ||
+                    specifier == 'X') {
                 switch (length) {
                 case LENGTH_CHAR:
                     value = (unsigned char) va_arg(ap, unsigned int);
@@ -171,11 +191,9 @@ int vcbprintf(void* param, size_t (*callback)(void*, const char*, size_t),
                 case LENGTH_PTRDIFF:
                     value = va_arg(ap, ptrdiff_t);
                     break;
-                default:
-                    value = 0;
                 }
-            } else if (specifier == SPECIFIER_SIGNED) {
-                intmax_t signedValue;
+            } else if (specifier == 'd' || specifier == 'i') {
+                intmax_t signedValue = 0;
                 switch (length) {
                 case LENGTH_CHAR:
                     signedValue = (signed char) va_arg(ap, int);
@@ -201,95 +219,96 @@ int vcbprintf(void* param, size_t (*callback)(void*, const char*, size_t),
                 case LENGTH_PTRDIFF:
                     signedValue = va_arg(ap, ptrdiff_t);
                     break;
-                default:
-                    signedValue = 0;
                 }
 
-                negative = signedValue < 0;
-                value = negative ? -signedValue : signedValue;
+                value = (uintmax_t) signedValue;
             }
 
             size_t size;
             char c;
             const char* s;
+            int written;
 
             switch (specifier) {
-            case SPECIFIER_SIGNED:
-                if (negative) {
-                    if (callback(param, "-", 1) != 1) return -1;
-                    result++;
+            case 'd': case 'i': case 'o': case 'u': case 'x': case 'X':
+                if (precision < 0) {
+                    precision = 1;
+                } else {
+                    flags &= ~FLAG_LEADING_ZEROS;
                 }
-                size = integerToString(buffer, value, 10, lowerDigits);
-                if (callback(param, buffer, size) != size) return -1;
-                result += size;
+
+                written = printInteger(param, callback, specifier, value,
+                        fieldWidth, precision, flags);
+                if (written < 0) return -1;
+                INCREMENT_RESULT(written);
                 break;
-            case SPECIFIER_OCTAL:
-                size = integerToString(buffer, value, 8, lowerDigits);
-                if (callback(param, buffer, size) != size) return -1;
-                result += size;
-                break;
-            case SPECIFIER_UNSIGNED:
-                size = integerToString(buffer, value, 10, lowerDigits);
-                if (callback(param, buffer, size) != size) return -1;
-                result += size;
-                break;
-            case SPECIFIER_HEX:
-                size = integerToString(buffer, value, 16, lowerDigits);
-                if (callback(param, buffer, size) != size) return -1;
-                result += size;
-                break;
-            case SPECIFIER_HEX_CAPITAL:
-                size = integerToString(buffer, value, 16, upperDigits);
-                if (callback(param, buffer, size) != size) return -1;
-                result += size;
-                break;
-            case SPECIFIER_CHAR:
+            case 'c':
                 c = (char) va_arg(ap, int);
-                if (callback(param, &c, 1) != 1) return -1;
-                result += 1;
+                written = printString(param, callback, &c, 1, fieldWidth,
+                        flags);
+                if (written < 0) return -1;
+                INCREMENT_RESULT(written);
                 break;
-            case SPECIFIER_STRING:
+            case 's':
                 s = va_arg(ap, const char*);
-                size = strlen(s);
-                if (callback(param, s, size) != size) return -1;
-                result += size;
+
+                if (precision < 0) {
+                    size = strlen(s);
+                } else {
+                    size = strnlen(s, (size_t) precision);
+                }
+                if (size > INT_MAX) goto overflow;
+
+                written = printString(param, callback, s, (int) size,
+                        fieldWidth, flags);
+                if (written < 0) return -1;
+                INCREMENT_RESULT(written);
                 break;
-            case SPECIFIER_POINTER:
+            case 'p':
                 value = (uintptr_t) va_arg(ap, void*);
-                size_t size = integerToString(buffer, value, 16, lowerDigits);
-                if (callback(param, buffer, size) != size) return -1;
-                result += size;
+                written = printInteger(param, callback, 'x', value, 0, 1,
+                        FLAG_ALTERNATIVE);
+                if (written < 0) return -1;
+                INCREMENT_RESULT(written);
                 break;
-            case SPECIFIER_N:
-                // TODO: Implement %n
+            case 'n':
+                *va_arg(ap, int*) = result;
                 break;
-            case SPECIFIER_PERCENT_SIGN:
+            case '%':
                 if (callback(param, "%", 1) != 1) return -1;
-                result += 1;
+                INCREMENT_RESULT(1);
                 break;
+            default:
+                // After finding an invalid (or unimplemented) conversion
+                // specifier, we no longer know where the next argument begins,
+                // so we will just print the remaining format string.
+                invalidConversion = true;
+                format = specifierBegin;
+                continue;
             }
         }
         format++;
     }
 
     return result;
+
+overflow:
+    errno = EOVERFLOW;
+    return -1;
 }
 
-static size_t integerToString(char* output, uintmax_t value, unsigned int base,
+static int integerToString(char* output, uintmax_t value, unsigned int base,
         const char* digits) {
     // Calculate the length of the output string
     uintmax_t i = value;
-    size_t length = 0;
+    int length = 0;
 
     while (i > 0) {
         i /= base;
         length++;
     }
 
-    if(length == 0) length = 1;
-
     output += length;
-    *output = '\0';
 
     do {
         *--output = digits[value % base];
@@ -302,4 +321,116 @@ static size_t integerToString(char* output, uintmax_t value, unsigned int base,
 static size_t noop(void* param, const char* s, size_t nBytes){
     (void) param; (void) s;
     return nBytes;
+}
+
+static int printInteger(void* param,
+        size_t (*callback)(void*, const char*, size_t), char specifier,
+        uintmax_t value, int fieldWidth, int precision, int flags) {
+    bool negative = false;
+
+    if (specifier == 'd' || specifier == 'i') {
+        intmax_t signedValue = (intmax_t) value;
+        if (signedValue < 0) {
+            negative = true;
+            value = (uintmax_t) -signedValue;
+        }
+    } else {
+        flags &= ~(FLAG_PLUS | FLAG_SPACE);
+    }
+
+    char buffer[(sizeof(uintmax_t) * 3)];
+    const char* digits = specifier == 'X' ? upperDigits : lowerDigits;
+    unsigned int base = 10;
+
+    if (specifier == 'o') {
+        base = 8;
+    } else if (specifier == 'x' || specifier == 'X') {
+        base = 16;
+    }
+
+    int stringLength = integerToString(buffer, value, base, digits);
+
+    if (flags & FLAG_ALTERNATIVE && specifier == 'o' &&
+            stringLength >= precision) {
+        if (__builtin_add_overflow(stringLength, 1, &precision)) {
+            errno = EOVERFLOW;
+            return -1;
+        }
+    }
+
+    int unpaddedLength = stringLength >= precision ? stringLength : precision;
+    if (negative || flags & (FLAG_PLUS | FLAG_SPACE)) {
+        if (__builtin_add_overflow(unpaddedLength, 1, &unpaddedLength)) {
+            errno = EOVERFLOW;
+            return -1;
+        }
+    }
+    if (flags & FLAG_ALTERNATIVE && (specifier == 'x' || specifier == 'X') &&
+            value != 0) {
+        if (__builtin_add_overflow(unpaddedLength, 2, &unpaddedLength)) {
+            errno = EOVERFLOW;
+            return -1;
+        }
+    }
+
+    if (!(flags & (FLAG_LEFT_JUSTIFIED | FLAG_LEADING_ZEROS))) {
+        for (int i = unpaddedLength; i < fieldWidth; i++) {
+            if (callback(param, " ", 1) != 1) return -1;
+        }
+    }
+
+    if (negative || flags & (FLAG_PLUS | FLAG_SPACE)) {
+        char sign = negative ? '-' : (flags & FLAG_PLUS) ? '+' : ' ';
+        if (callback(param, &sign, 1) != 1) return -1;
+    }
+
+    if (flags & FLAG_ALTERNATIVE) {
+        if (specifier == 'x' && value != 0) {
+            if (callback(param, "0x", 2) != 2) return -1;
+        } else if (specifier == 'X' && value != 0) {
+            if (callback(param, "0X", 2) != 2) return -1;
+        }
+    }
+
+    if (!(flags & FLAG_LEFT_JUSTIFIED) && flags & FLAG_LEADING_ZEROS) {
+        for (int i = unpaddedLength; i < fieldWidth; i++) {
+            if (callback(param, "0", 1) != 1) return -1;
+        }
+    }
+
+    for (int i = stringLength; i < precision; i++) {
+        if (callback(param, "0", 1) != 1) return -1;
+    }
+
+    if (callback(param, buffer, stringLength) != (size_t) stringLength) {
+        return -1;
+    }
+
+    if (flags & FLAG_LEFT_JUSTIFIED) {
+        for (int i = unpaddedLength; i < fieldWidth; i++) {
+            if (callback(param, " ", 1) != 1) return -1;
+        }
+    }
+
+    return unpaddedLength >= fieldWidth ? unpaddedLength : fieldWidth;
+}
+
+static int printString(void* param,
+        size_t (*callback)(void*, const char*, size_t), const char* s,
+        int length, int fieldWidth, int flags) {
+    if (!(flags & FLAG_LEFT_JUSTIFIED)) {
+        for (int i = length; i < fieldWidth; i++) {
+             if (callback(param, " ", 1) != 1) return -1;
+        }
+    }
+
+    if (callback(param, s, length) != (size_t) length) return -1;
+
+    if (flags & FLAG_LEFT_JUSTIFIED) {
+        for (int i = length; i < fieldWidth; i++) {
+            if (callback(param, " ", 1) != 1) return -1;
+        }
+    }
+
+    return length >= fieldWidth ? length : fieldWidth;
 }
