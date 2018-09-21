87ac1c6b60afbf5797bf7b5b31292e15991e5759
Dennis Wölfing <denniswoelfing@gmx.de>
2018-09-21 14:53:50 +0200

    Add some <string.h> functions.
    Add memchr(3), strcat, strncat, strncpy, strpbrk and strstr.

--- libc/include/string.h
+++ libc/include/string.h
@@ -29,10 +29,12 @@
 extern "C" {
 #endif
 
+void* memchr(const void*, int, size_t);
 int memcmp(const void*, const void*, size_t);
 void* memcpy(void* __restrict, const void* __restrict, size_t);
 void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);
+char* strcat(char* __restrict, const char* __restrict);
 char* strchr(const char*, int);
 int strcmp(const char*, const char*);
 int strcoll(const char*, const char*);
@@ -40,9 +42,13 @@ char* strcpy(char* __restrict, const char* __restrict);
 size_t strcspn(const char*, const char*);
 char* strerror(int);
 size_t strlen(const char*);
+char* strncat(char* __restrict, const char* __restrict, size_t);
 int strncmp(const char*, const char*, size_t);
+char* strncpy(char* __restrict, const char* __restrict, size_t);
+char* strpbrk(const char*, const char*);
 char* strrchr(const char*, int);
 size_t strspn(const char*, const char*);
+char* strstr(const char*, const char*);
 char* strtok(char* __restrict, const char* __restrict);
 
 #if __USE_DENNIX || __USE_POSIX
