143a84fbd46158f846ce4a5b20bb78afbf540b95
Dennis Wölfing <denniswoelfing@gmx.de>
2019-08-07 15:36:20 +0200

    Release Dennix 0.4.

--- build-aux/version.mk
+++ build-aux/version.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2016, 2017, 2018 Dennis Wölfing
+# Copyright (c) 2016, 2017, 2018, 2019 Dennis Wölfing
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -12,4 +12,4 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-VERSION = 0.4dev
+VERSION = 0.4
