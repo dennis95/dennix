4f2849838b0cc28519da4abb1249eec7c3268aba
Dennis Wölfing <denniswoelfing@gmx.de>
2020-02-23 18:05:28 +0100

    Set TERM environment variable.
    In graphics mode TERM is set to "dennix", in VGA text mode it is set to
    "dennix-16color".

--- utils/init.c
+++ utils/init.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017, 2019 Dennis Wölfing
+/* Copyright (c) 2017, 2019, 2020 Dennis Wölfing
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -17,11 +17,14 @@
  * System initialization.
  */
 
+#include <devctl.h>
 #include <err.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <dennix/display.h>
 
 int main(int argc, char* argv[]) {
     (void) argc; (void) argv;
@@ -29,6 +32,21 @@ int main(int argc, char* argv[]) {
     if (getpid() != 1) errx(1, "PID is not 1");
 
     if (setenv("PATH", "/bin:/sbin", 1) < 0) err(1, "setenv");
+
+    const char* term = "dennix-16color";
+    int fd = open("/dev/display", O_RDONLY);
+    if (fd >= 0) {
+        struct display_resolution res;
+        // Check whether we are running in graphics mode. In text mode this call
+        // fails with ENOTSUP.
+        if (posix_devctl(fd, DISPLAY_GET_RESOLUTION, &res, sizeof(res),
+                NULL) == 0) {
+            term = "dennix";
+        }
+        close(fd);
+    }
+
+    if (setenv("TERM", term, 1) < 0) err(1, "setenv");
 
     pid_t childPid = fork();
     if (childPid < 0) err(1, "fork");
