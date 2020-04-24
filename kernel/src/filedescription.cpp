b24257cc4152e7e5dbf83beb0da7a36a9551cba4
Dennis Wölfing <denniswoelfing@gmx.de>
2020-04-24 11:44:07 +0200

    Do not follow symlinks for O_EXCL.

--- kernel/src/filedescription.cpp
+++ kernel/src/filedescription.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016, 2017, 2018, 2019 Dennis Wölfing
+/* Copyright (c) 2016, 2017, 2018, 2019, 2020 Dennis Wölfing
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -68,7 +68,7 @@ Reference<FileDescription> FileDescription::openat(const char* path, int flags,
         mode_t mode) {
     const char* name;
     Reference<Vnode> parentVnode = resolvePathExceptLastComponent(vnode, path,
-            &name, !(flags & O_NOFOLLOW));
+            &name, !(flags & (O_EXCL | O_NOFOLLOW)));
     if (!parentVnode) return nullptr;
     Reference<Vnode> vnode = parentVnode;
     if (!*name) name = ".";
